6698ca683360288c02374f3d04aee37b56e652a9
kento kadowaki <kadowakikento@gmail.com>
2022-07-27 01:29:19 +0900

    通知メールが飛ぶのを実装

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -51,6 +51,7 @@ class TeamsController < ApplicationController
   def assign_owner
     @team.update(owner_id: params[:owner_id])
     @user = User.find(@team.owner_id)
+    ContactMailer.contact_mail(@user).deliver 
     redirect_to team_path, notice: 'オーナーの権限が移動しました'
 
   end
